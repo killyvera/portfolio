d12717415b076d8444966bced23b94a018ba57dd
killyvera <david.esmeralda@gmail.com>
2022-07-10 15:09:40 -0500

    add models to sanity

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -3,6 +3,14 @@ import createSchema from 'part:@sanity/base/schema-creator'
 
 // Then import schema types from any plugins that might expose them
 import schemaTypes from 'all:part:@sanity/base/schema-type'
+import abouts from './abouts'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import skills from './skills'
+import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -11,6 +19,14 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
+    abouts,
+    brands,
+    contact,
+    experiences,
+    skills,
+    testimonials,
+    workExperience,
+    works
     /* Your types here! */
   ]),
 })
