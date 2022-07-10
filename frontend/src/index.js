a545e204d2d488974b8ff2b17bad5013c4d4f5b1
killyvera <david.esmeralda@gmail.com>
2022-07-10 15:42:28 -0500

    add  new react dom handler

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as ReactDOMClient from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const container = document.getElementById('root');
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App tab="home" />);
