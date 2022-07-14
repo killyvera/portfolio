b0e0b37f2652cbf0c5fc0ef9f7a1d58dc6cbaae2
killyvera <david.esmeralda@gmail.com>
2022-07-14 02:22:57 -0500

    added sanity client

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -30,7 +30,7 @@ const abouts = [
 const About = () => {
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
