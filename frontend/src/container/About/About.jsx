7aef70a434d34c6184a23116e28c8cfa443d54c6
killyvera <david.esmeralda@gmail.com>
2022-07-21 01:49:54 -0500

    add MotionWrap to About container

--- frontend/src/container/About/About.jsx
+++ frontend/src/container/About/About.jsx
@@ -1,26 +1,24 @@
-import React, { useState, useEffect } from "react";
-import { motion } from "framer-motion";
-import {AppWrap} from '../../wrapper'
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 
-
-import {images} from '../../constants';
-import "./About.scss";
-import { urlFor, client } from "../../client"
+import { AppWrap, MotionWrap } from '../../wrapper';
+import './About.scss';
+import { urlFor, client } from '../../client';
 
 const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    const query ='*[_type == "abouts"]';
+    const query = '*[_type == "abouts"]';
 
-    client.fetch(query)
-    .then((data) => { setAbouts(data) } )
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
   }, []);
-
 
   return (
     <>
-      <h2 className="head-text">I Know that <span>Good Development</span> <br />means  <span>Good Business</span></h2>
+      <h2 className="head-text">I Know that <span>Good Design</span> <br />means  <span>Good Business</span></h2>
 
       <div className="app__profiles">
         {abouts.map((about, index) => (
@@ -41,4 +39,8 @@ const About = () => {
   );
 };
 
-export default AppWrap(About, 'about');
+export default AppWrap(
+  MotionWrap(About, 'app__about'),
+  'about',
+  'app__whitebg',
+);
